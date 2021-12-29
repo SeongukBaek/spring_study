f94f49c8cf3a90e4dd5818cbbf75eb3d61363bb1
SeongukBaek <bsu0541@naver.com>
2021-12-29 16:18:26 +0900

    회원 서비스 개발 중

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,41 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private int pwd;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(int pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
