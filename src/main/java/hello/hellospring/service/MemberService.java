258cd2afdfb83103444e08b75294cc37d432a08c
SeongukBaek <bsu0541@naver.com>
2022-01-02 16:12:25 +0900

    회원관리예제

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,5 +1,49 @@
 package hello.hellospring.service;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
 
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 같은 이름인 중복회원을 불가능해야 함
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        ValidateDuplicateMember(member); // 이름 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // ctrl T - Extract method
+    private void ValidateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 특정 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
